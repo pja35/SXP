192978db3ead5e02a1f3ab82a89f54ff0a2192c2
Nathanaël <nathanael.eon@centrale-marseille.fr>
2016-11-04 16:19:56 +0100

    modifié :         src/main/js/html/messages.html 	modifié :         src/main/js/modules/messages.js

--- src/main/js/modules/messages.js
+++ src/main/js/modules/messages.js
@@ -31,20 +31,22 @@
 	module.controller('display', function($scope){
 		
 		//TODO: Make this automatic with a connexion to the server
+		var currentUser=0;
+		
 		this.users=[
-			{"see":true,"id":0,"name":"User 1", "activity":"active", "mails":[
+			{"show":true,"id":0,"name":"User 1", "activity":"active", "mails":[
 					{"date":"2 hours ago","text":"carrot ?"},
 					{"date":"3 hours ago","text":"potatoes ?"}
 				]},
-			{"see":false,"id":1,"name":"User 2", "activity":"", "mails":[
+			{"show":false,"id":1,"name":"User 2", "activity":"", "mails":[
 					{"date":"1 hours ago","text":"Hey !"},
 					{"date":"4 hours ago","text":"You there?"}
 				]},
-			{"see":false,"id":2,"name":"User 3", "activity":"", "mails":[
+			{"show":false,"id":2,"name":"User 3", "activity":"", "mails":[
 					{"date":"1 hours ago","text":"Hey 2!"},
 					{"date":"4 hours ago","text":"You there 2?"}
 				]},
-			{"see":false,"id":3,"name":"User 4", "activity":"", "mails":[
+			{"show":false,"id":3,"name":"User 4", "activity":"", "mails":[
 					{"date":"1 hours ago","text":"Hey 3!"},
 					{"date":"4 hours ago","text":"You there3?"}
 				]}			 
@@ -52,9 +54,11 @@
 		
 		//Function that changes the view when a link is clicked
 		this.toggleUserActivity = function(j){
-			for (var i=0; i<this.users.length;i++){this.users[i].activity=""; this.users[i].see=false};
+			this.users[currentUser].activity="";
+			this.users[currentUser].show=false;
+			currentUser=j;
 			this.users[j].activity="active";
-			this.users[j].see=true;
+			this.users[j].show=true;
 		}
 	});
 	
