afe9e0b6e8156985efc4225ed3bfb8de23a7ba37
Nathanaël <nathanael.eon@centrale-marseille.fr>
2016-11-04 16:30:48 +0100

    modifié :         src/main/js/modules/messages.js

--- src/main/js/modules/messages.js
+++ src/main/js/modules/messages.js
@@ -35,22 +35,18 @@
 
 		//TODO: Make this automatic with a connexion to the server
 		this.users=[
-			{"show":true,"id":0,"name":"User 1", "activity":"active", "mails":[
+			{"show":true,"id":0,"name":"Bob", "activity":"active", "mails":[
 					{"date":"2 hours ago","text":"carrot ?"},
 					{"date":"3 hours ago","text":"potatoes ?"}
 				]},
-			{"show":false,"id":1,"name":"User 2", "activity":"", "mails":[
-					{"date":"1 hours ago","text":"Hey !"},
-					{"date":"4 hours ago","text":"You there?"}
+			{"show":false,"id":1,"name":"Alice", "activity":"", "mails":[
+					{"date":"1 hours ago","text":"You there?"},
+					{"date":"1 hours ago","text":"Hey!"}
 				]},
 			{"show":false,"id":2,"name":"User 3", "activity":"", "mails":[
 					{"date":"1 hours ago","text":"Hey 2!"},
 					{"date":"4 hours ago","text":"You there 2?"}
-				]},
-			{"show":false,"id":3,"name":"User 4", "activity":"", "mails":[
-					{"date":"1 hours ago","text":"Hey 3!"},
-					{"date":"4 hours ago","text":"You there3?"}
-				]}			 
+				]},		 
 		];
 		
 		//Function that changes the view when we change the conversation is clicked
